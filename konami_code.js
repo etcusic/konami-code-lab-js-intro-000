f19596e0782fb44e6883dbb8ad9ce136a0de323e
Eli Cusic <tntitans31@gmail.com>
2020-06-10 11:50:41 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+var event = document.body.addEventListener("keydown", init)
+
+var accumulator = 0
+
+let init = function (event) {
+  if (event.key !== codes[accumulator]){
+    accumulator = 0
+    return
+  }
+
+  accumulator++
+
+  if (codes.length === accumulator){
+    window.alert("Hurray!")
+  }
 }
+
+// var event = document.body.addEventListener("keydown", init)
